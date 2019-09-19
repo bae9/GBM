43bae2fbc1b728bc379721032c4551fdb44b9353
bae9 <55287788+bae9@users.noreply.github.com>
2019-09-19 11:32:33 -0400

    Update Universal IV.R
    Added text

--- Universal IV.R
+++ Universal IV.R
@@ -4,6 +4,10 @@ qty <- aggregate(myData$Qty, by=list(Category=myData$Arm.Group.Title), FUN=sum)
 numpeople <- aggregate(myData$X..People, by=list(Category=myData$Arm.Group.Title), FUN=mean)
 cat <- aggregate(myData$Universal.IV, by=list(Category=myData$Arm.Group.Title), FUN=mean)
 df <- data.frame(qty, numpeople, cat)
-boxplot(df$x/df$x.1 ~ as.logical(df$x.2), data=myData,horizontal=TRUE, ylab="Use of IV", xlab="% Patient Dropout")
+bxp <- boxplot(df$x/df$x.1 ~ as.logical(df$x.2), data=myData,horizontal=TRUE, ylab="Use of IV", xlab="% Patient Dropout")
+text(x=0.1, y=1, labels=toString(round(bxp$stats[3,1] * 100, digits=2)))
+text(x=0.2, y=2, labels=toString(round(bxp$stats[3,2] * 100, digits=2)))
 title(paste("% Patient Dropout rate by use of Universal IV \n n=", sum(numpeople$x), sep=""))
+
+
 
